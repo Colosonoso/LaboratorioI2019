c46da9e0d85cfbb073e2b2dde786ab27738fc874
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2019-04-11 12:21:34 -0300

    Add files via upload

---
+++ numero Nico/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int numero;
+
+    printf("Ingrese un numero");
+    scanf("%d",&numero);
+
+    if(numero<=0)
+    {
+
+       printf("Nico le gusta que le den por la oreja");
+
+    }
+    else
+    {
+
+      printf("Le gusta la banana por donde no le da el sol");
+
+    }
+
+
+    return 0;
+}
